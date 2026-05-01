a748e2615602d647ddcd0d9df79a741b83bedf0d
Filip Radzikowski <filipr667@gmail.com>
2026-05-01 23:17:01 +0100

    fix: use server.listen instead of app.listen

--- server.js
+++ server.js
@@ -391,6 +391,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+service.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
